e7401cbd98baf3b617a5b5cdd969b2cdef73abbd
Md Repon <repon7253@gmail.com>
2025-12-24 00:31:29 +0600

    fix issue

--- src/app/(commonLayout)/settings/page.tsx
+++ src/app/(commonLayout)/settings/page.tsx
@@ -1,11 +1,6 @@
 'use client';
 
 import UnderTheConstruction from '@/components/modules/UnderTheConstruction/UnderTheConstruction';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'settings',
-};
 
 const SettingsPage = () => {
   return <UnderTheConstruction />;
